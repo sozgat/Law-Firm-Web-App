e8efa5257735ee7de2c4f7d4d39c98989b908a56
mehmetagca <mehmet.agca@yandex.com>
2017-04-24 03:40:34 +0200

    kayitKontrol degisim KayitKontrol

--- WebApplication1/src/java/net/serhatmercan/jsf/kayitKontrol.java
+++ WebApplication1/src/java/net/serhatmercan/jsf/kayitKontrol.java
@@ -1,18 +1,24 @@
 
 package net.serhatmercan.jsf;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean
+@ManagedBean(name = "BeanKayitKontrol") // Bean ismi
 @SessionScoped
 
-public class kayitKontrol {
+public class KayitKontrol { // Class isimleri buyuk harfle basliyor.
        
     private String tcKimlikNo; // TC Kimlik No üzerinde işlem yapmayacağımız için String tanımladım. Değişim yapılabilir.
     private String ad;
     private String soyad;
-    private String dogumTarihi;
+    private String dogumTarihi; // Dogum tarihini date felan mı yapsak, veritabanında ben date yapıcam oraya kaydederken sorun yasarmıyız?(Mehmet)
     private String buroAdi;
     private int buroNo;
     private String kullaniciAdi;
@@ -91,6 +97,52 @@ public class kayitKontrol {
     public void setTcKimlikNo(String tcKimlikNo) {
         this.tcKimlikNo = tcKimlikNo;
     }
+    
+    public String kaydet()
+    {
+        PreparedStatement ps = null;
+        Connection baglanti = DbFunctions.getCon();
+        
+        if(baglanti == null)
+        {
+            //(Mehmet)Kullaniciya Veritabanina Baglanti Hatasi mesaji gosterelim. said sayfanin biyerine textbox mi eklersin duruma gore buraya ekleriz.
+            return "kayitol.xhtml";
+        }
+        String eklemeKodu = "INSERT INTO TBLKULLANICILAR2"//---!---(Mehmet)DB de duzeltme yaptıktan sonra isimdeki 2 yi silicez.
+                + "(ad,soyad,dogumTarih,tcKimlikNo,kullaniciAd,sifre,email,buroNo) "
+                + "VALUES('"+ getAd() +"','"+ getSoyad() +"',"+ DbFunctions.stringToDate(getDogumTarihi())
+                + ",'"+ getTcKimlikNo() +"','"+ getKullaniciAdi() +"','"+ getSifre() +"','"+ getEmail() +"',"+ getBuroNo() +")";
+        
+        try {
+          /*Hocanin yaptigi gibi yapmaya calistim fakat sql komutu calismadi, duzeltebilirsek
+            yorum satirina aldigim bu satirlari tekrar kullaniriz.(Mehmet)
+            ps.setString(1, getAd());
+            ps.setString(2, getSoyad());
+            ps.setString(3, DbFunctions.stringToDate(getDogumTarihi()));
+            ps.setString(4, "d");
+            ps.setString(5, "s");
+            ps.setString(6, "s");
+            ps.setString(7, "d");
+            ps.setInt(8, getBuroNo());
+            */
+            ps = baglanti.prepareStatement(eklemeKodu);
+            ps.execute();
+            System.out.println("Kayit eklendi.");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(KayitKontrol.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Kayit sirasinda hata olustu!");//(Mehmet)HTML kismindada Hata mesaji verebilriiz.
+            return "kayitol.xhtml";
+        }
+        finally
+        {
+            DbFunctions.baglantiKapa(baglanti);
+            return "kayitKontrolJsf.xhtml";
+        }
+        
+    }
+    
+    
     
     
 }
